f4b9ec622f54caef731e4e65bec7fbc60734e906
sooshiance <s.katafract@gmail.com>
2025-03-27 09:07:51 +0330

    add unordered numbers for array

--- src/data_structure/array.rs
+++ src/data_structure/array.rs
@@ -1,14 +1,29 @@
 pub fn arrays() {
-    let numbers: [i32; 10] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    let ordered_number: [i32; 10] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    let unordered_number: [i32; 10] = [100, 61, 25, 34, 53, 27, 91, 88, 76, 11];
 
-    println!("The first number is: {}", numbers[0]);
-    println!("The second number is: {}", numbers[1]);
+    println!("The first number is: {}", ordered_number[0]);
+    println!("The second number is: {}", ordered_number[1]);
 
-    let length = numbers.len();
+    let length = ordered_number.len();
     println!("The length of the array is: {}", length);
 
-    println!("All numbers in the array:");
-    for &number in &numbers {
-        println!("{}", number);
+    println!("All ordered_number in the array:");
+    for &number in &ordered_number {
+        print!("{}\t", number);
     }
+
+    println!();
+    
+    println!("The first number is: {}", unordered_number[0]);
+    println!("The second number is: {}", unordered_number[1]);
+
+    let length = unordered_number.len();
+    println!("The length of the array is: {}", length);
+
+    println!("All unordered_number in the array:");
+    for &number in &unordered_number {
+        print!("{}\t", number);
+    }
+    println!();
 }
